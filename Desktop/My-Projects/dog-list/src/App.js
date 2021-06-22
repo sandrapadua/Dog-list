5ed01efde0605012544a27b87e65875ee8d7fc82
sandrapadua <sandrapadua5@gmail.com>
2021-06-22 17:45:14 +0530

    set-up-the-ptoject

---
+++ Desktop/My-Projects/dog-list/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Doglist from './Doglist'
+function App() {
+  return (
+    <div className="App">
+     <Doglist/> 
+    </div>
+  );
+}
+
+export default App;
